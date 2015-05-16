a0cdfdf3c0f1575c95739816ac10f954f033923e
Stephen Spencer <stephen-nhsmaphack@vitaminj.co.uk>
2015-05-16 18:00:51 +0100

    Do geolocation automatically

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,32 +7,34 @@ interface GeoJsonPoint {
 	coordinates: string;
 }
 
+var EMOJI_SADFACE:string = '&#128542;';
+var EMOJI_TARGET:string = '&#127919;';
+var EMOJI_SEARCH:string = '&#128269;';
+
+var currentPosition:Position;
+
 $(function() {
 	
 	function geolocSuccess(position:Position) {
-		$('#search').val(position.coords.latitude +","+ position.coords.longitude);
-	}
-	
-	function geolocError(err:PositionError) {
-		console.log("error", err);
-	}
-	
-	if (navigator.geolocation)
-		$('button.dogeoloc').removeAttr('disabled');
-	
-	$('button.dogeoloc').click(function() {
-		navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
-	});
-	
-	$('button.dosearch').click(function() {
-		$('button.dosearch').html('&#9203;').attr('disabled');  //wait cursor
-		$.ajax("http://crowdhealth.herokuapp.com/api/v1/types/Defibrillator").done(function(data) {
-			$('button.dosearch').html('&#128269;').removeAttr('disabled');  //search icon
+		$('#status').html(EMOJI_SEARCH);
+		currentPosition = position;
+		var queryParams:any = {lat:position.coords.latitude,lng:position.coords.longitude};
+		$.ajax("http://crowdhealth.herokuapp.com/api/v1/types/Defibrillator/nearest/",{data:queryParams}).done(function(data) {
+			$('#status').empty();
 			$('#results').empty();
 			maximum = 5;
 			addResultsFromGeoJson(data);
 		})
-	});
+	}
+	
+	function geolocError(err:PositionError) {
+		$('#status').html(EMOJI_SADFACE);
+	}
+	
+	if (navigator.geolocation) {
+		$('#status').html(EMOJI_TARGET);
+		navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+	}
 	
 	var maximum:number;
 	
@@ -40,15 +42,19 @@ $(function() {
 		if (maximum < 0) return;
 		maximum--;
 		var result:JQuery = $($('#TMPL_RESULT').html());
-		var latlon:string = point[1]+','+point[0];
+		var latLon:string = point[1]+','+point[0];
+		var curLatLon:string = currentPosition.coords.latitude+','+currentPosition.coords.longitude;
 		result.find('.title').html(title);
 		result.find('.description').html(description);
-		result.find('img').attr('src','https://maps.googleapis.com/maps/api/staticmap?center='+latlon+'&zoom=17&size=200x200&markers='+latlon);
+		result.find('img').attr('src','https://maps.googleapis.com/maps/api/staticmap?center='+latLon+'&zoom=17&size=200x200&markers='+latLon);
+		var uri:string = 'http://maps.apple.com/?daddr='+latLon+'&saddr='+curLatLon;
+		if (navigator.userAgent.indexOf('(Android;') != -1)
+			uri = 'geo:'+latLon+'?q='+latLon;
+		result.find('a').prepend(EMOJI_TARGET).attr('href', uri);
 		$('#results').append(result);
 	}
 	
 	function addResultsFromGeoJson(geojson:any) {
-		//console.log(geojson);
 		if (geojson.type == "FeatureCollection")
 			geojson.features.forEach(addResultsFromGeoJson);
 		else if ((geojson.type == "Feature") && (geojson.geometry.type == "Point")) {
